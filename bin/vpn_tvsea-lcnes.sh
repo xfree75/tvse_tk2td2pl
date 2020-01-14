0c8caa29ac7d03b271825e004e84464284b396cd
Jinook Kim <xfree75@gmail.com>
2020-01-15 07:39:52 +0900

    vpn

--- bin/vpn_tvsea-lcnes.sh
+++ bin/vpn_tvsea-lcnes.sh
@@ -4,14 +4,18 @@ set -x
 
 sudo systemctl stop plexpy.service
 
-sudo ipsec up vpn-tor2 \
+sudo ipsec up vpn-sf2 \
     && sleep 10 \
     && tvsea-lcnes.py \
     && sleep 80 \
     
-sudo ipsec down vpn-tor2
-sleep 20
+sudo ipsec down vpn-sf2
+sleep 8
 sudo systemctl restart plexmediaserver.service
-sleep 20
+sleep 8
+sudo systemctl restart dnsmasq.service
+sleep 8
 sudo systemctl start plexpy.service
+
+
 
