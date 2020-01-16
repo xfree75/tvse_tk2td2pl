a01bdf3ad1b2f5bb7ebcef0851a59a122119a1f7
Jinook Kim <xfree75@gmail.com>
2020-01-16 22:17:08 +0900

    add file executable mode

--- bin/vpn_tvsea-lcnes.sh
+++ bin/vpn_tvsea-lcnes.sh
@@ -4,12 +4,12 @@ set -x
 
 sudo systemctl stop plexpy.service
 
-sudo ipsec up vpn-sf2 \
+sudo ipsec up vpn-hk \
     && sleep 10 \
     && tvsea-lcnes.py \
     && sleep 80 \
     
-sudo ipsec down vpn-sf2
+sudo ipsec down vpn-hk
 sleep 8
 sudo systemctl restart plexmediaserver.service
 sleep 8
